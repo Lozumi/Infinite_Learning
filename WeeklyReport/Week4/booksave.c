90927bd3bd99030eb1300b346eab1bf42f176e0d
Lozumi Everkine <fantasynico1026@gmail.com>
2023-02-25 19:53:34 +0800

    vault backup: 2023-02-25 19:53:34

--- WeeklyReport/Week4/booksave.c
+++ WeeklyReport/Week4/booksave.c
@@ -1,2 +1,23 @@
 // booksave.c -- 在文件中保存结构中的内容
-#include
+#include <stdio.h>
+#include <string.h>
+#define TSIZE 45
+#define FMAX 5
+
+struct film
+{
+    char title[TSIZE];
+    int rating;
+};
+char *s_gets(char *st, int n);
+int main()
+{
+    struct film movies[FMAX];
+    int i = 0;
+    int j;
+
+    puts("Enter 1st movie title:");
+    while (i < FMAX && s_gets(movies[i].title, TSIZE) != NULL && movie[i].title[0] != '\0')
+    {
+    }
+}
