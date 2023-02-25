f1795a5c1b9b6591ce87157f2f47bbb9a9fef337
Lozumi Everkine <fantasynico1026@gmail.com>
2023-02-25 20:04:34 +0800

    vault backup: 2023-02-25 20:04:34

--- WeeklyReport/Week4/booksave.c
+++ WeeklyReport/Week4/booksave.c
@@ -14,10 +14,41 @@ int main()
 {
     struct film movies[FMAX];
     int i = 0;
-    int j;
 
     puts("Enter 1st movie title:");
-    while (i < FMAX && s_gets(movies[i].title, TSIZE) != NULL && movie[i].title[0] != '\0')
+    while (i < FMAX && s_gets(movies[i].title, TSIZE) != NULL && movies[i].title[0] != '\0')
     {
+        puts("Enter rating:");
+        scanf("%d", &movies[i++].rating);
+        while (getchar() != '\n')
+            continue;
+        puts("Enter next movie title:(Empty line to quite)");
     }
+    if (i == 0)
+        printf("No data entered.");
+    else
+        printf("Your movie list:\n");
+
+    for (int j = 0; j < i; j++)
+        printf("Movie: %10s Rating: %5d", movies[j].title, movies[j].rating);
+    printf("Sayonala~");
+
+    return 0;
+}
+
+char *s_gets(char *st, int n)
+{
+    char *ret_val;
+    char *find;
+    ret_val = fgets(st, n, stdin);
+    if (ret_val) // 即非空
+    {
+        find = strchr(st, '\n');
+        if (find) // 即非空
+            *find = '\0';
+        else
+            while (getchar() != '\n')
+                continue; // 处理剩余输入行
+    }
+    return ret_val;
 }
